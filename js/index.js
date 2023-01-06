769cf618de4b90aa66510f59dc6467b8727b598d
MitchellV3 <30252114+MitchellV3@users.noreply.github.com>
2023-01-06 02:00:35 -0700

    Create grid of divs

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 const boxContainer = document.querySelector(".box-container");
-const createDiv = document.createElement("div");
 
-const rowContainer = document.createElement("div");
-rowContainer.classList.add("row-container");
-boxContainer.appendChild(rowContainer);
+//const rowContainer = document.createElement("div");
+//rowContainer.classList.add("row-container");
+//boxContainer.appendChild(rowContainer);
 
-for (let i = 0; i < 16; i++) {
-    rowBox = document.createElement("div");
-    rowBox.id = 'b'+i;
-    rowBox.classList.add("row-box");
-    rowContainer.appendChild(rowBox);
+let rowArray = [];
+
+numberOfRows = () => {
+    for (let i = 0; i < 16; i++) {
+        const rowContainer = document.createElement("div");
+        rowContainer.classList.add("row-container");
+        boxContainer.appendChild(rowContainer);
+        rowArray.push(rowContainer);
+        }
+}
+numberOfRows();
+for (let i = 0; i < rowArray.length; i++) {
+    for (let i = 0; i < rowArray.length; i++) {
+        rowBox = document.createElement("div");
+        rowBox.classList.add("row-box");
+        let currentRow = rowArray[i]; 
+        currentRow.appendChild(rowBox);
+    }
 }
