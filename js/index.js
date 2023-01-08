4e43a1d3a17cce8a03cf4441f6beca1bd724a8ec
MitchellV3 <30252114+MitchellV3@users.noreply.github.com>
2023-01-07 19:09:39 -0700

    Add a button to change the size of the board

--- js/index.js
+++ js/index.js
@@ -1,33 +1,47 @@
 const boxContainer = document.querySelector(".box-container");
+const rowArray = [];
 
-//const rowContainer = document.createElement("div");
-//rowContainer.classList.add("row-container");
-//boxContainer.appendChild(rowContainer);
+fillRows = (size) => {
 
-let rowArray = [];
+    for (let i = 0; i < rowArray.length; i++) {
+        for (let i = 0; i < size; i++) {
+            rowBox = document.createElement("div");
+            rowBox.classList.add("row-box");
+            let currentRow = rowArray[i]; 
+            currentRow.appendChild(rowBox);
+        }
+    }
 
-numberOfRows = () => {
-    for (let i = 0; i < 16; i++) {
+} 
+
+createRows = (size = 16) => {
+
+    for (let i = 0; i < size; i++) {
         const rowContainer = document.createElement("div");
         rowContainer.classList.add("row-container");
         boxContainer.appendChild(rowContainer);
         rowArray.push(rowContainer);
         }
-}
-numberOfRows();
-for (let i = 0; i < rowArray.length; i++) {
-    for (let i = 0; i < rowArray.length; i++) {
-        rowBox = document.createElement("div");
-        rowBox.classList.add("row-box");
-        let currentRow = rowArray[i]; 
-        currentRow.appendChild(rowBox);
-    }
+    fillRows(size);
+
 }
 
-let box = document.querySelectorAll('.row-box');
-let reset = document.querySelector('.reset-btn');
-let size = document.querySelector('.size-btn');
+createRows();
 
+const box = document.getElementsByClassName('row-box');
+const reset = document.querySelector('.reset-btn');
+const size = document.querySelector('.size-btn');
+
+for (let i = 0; i < box.length; i++) {
+    box[i].addEventListener('mouseenter', (e) =>{
+        box[i].style.backgroundColor = "rgb(12, 12, 12)";
+    })
+    reset.addEventListener('click', (e) => {
+        box[i].style.backgroundColor = "";
+    })
+}
+
+/*
 box.forEach(function(box){
         box.addEventListener('mouseenter', (e) =>{
             box.style.backgroundColor = "rgb(12, 12, 12)";
@@ -35,6 +49,28 @@ box.forEach(function(box){
         reset.addEventListener('click', (e) => {
             box.style.backgroundColor = "";
         })
+})
+*/
+size.addEventListener('click', (e) => {
+    const size = prompt("Enter the desired number of boxes (maximum 100)");
+      
+    if (size > 100 || size < 1) {
+        return "Value out of bounds";
+    } else if (isNaN(size)) {
+        return "Value is not a number";
+    } else {
+        rowArray.length = 0;
+        boxContainer.innerHTML = "";
+        createRows(size );
+        for (let i = 0; i < box.length; i++) {
+            box[i].addEventListener('mouseenter', (e) =>{
+                box[i].style.backgroundColor = "rgb(12, 12, 12)";
+            })
+            reset.addEventListener('click', (e) => {
+                box[i].style.backgroundColor = "";
+            })
+    }
+    }  
 })
 
 
